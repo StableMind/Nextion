48ff1b93e937bb71ac67b4477e719c9a865a8e13
Jez <londonvibe@gmail.com>
2018-04-14 22:30:13 +0100

    Update nextionPi.py

--- nextionPi.py
+++ nextionPi.py
@@ -1,1 +1,22 @@
+#import modules
+import serial
+#Set end of file
+eof = "\xff\xff\xff"
 
+#setup connection
+ser = serial.Serial(
+
+    port='/dev/ttyAMA0',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+
+#loop waiting for hex input
+while 1:
+    #get the data from the nextion display
+    x = ser.readline()
+    #print output with repr (returns a string containing a printable representation of an object)
+    print "Recieved:" + repr(x)
