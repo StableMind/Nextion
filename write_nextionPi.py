34b7107bb198d8929c1a46d1b6143e5622744f50
Jez <londonvibe@gmail.com>
2018-04-15 22:36:03 +0100

    Write to the Nextion Display

--- write_nextionPi.py
+++ write_nextionPi.py
@@ -1,8 +1,10 @@
 
 #import module: install with 'easy_install -U pyserial'
 import serial
+
 #Set end of file
 eof = "\xff\xff\xff"
+
 #setup text for writing
 txt = 'Hello wold'
 
@@ -14,7 +16,7 @@ con = serial.Serial(
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
-    
 )
+
 #write text to Page 0 t0 txt variable(check the id of your text box) plus EOF
 con.write('page0.t0.txt='txt+eof)
